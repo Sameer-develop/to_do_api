476f7b124b786a421fc1576b3134e19206c7bb3e
Sameer-develop <sameer.10xtech@gmail.com>
2025-09-08 14:04:17 +0530

    routes.py
    This is the 6th part of code in dev branch

---
+++ routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.schemas.person_schema import PersonCreate, PersonResponse
+from app.services import person_service
+from app.core.database import SessionLocal
+
+router = APIRouter()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/", response_model=PersonResponse)
+def create_person(person: PersonCreate, db: Session = Depends(get_db)):
+    return person_service.create_person(db, person)
+
+@router.get("/", response_model=list[PersonResponse])
+def read_persons(db: Session = Depends(get_db)):
+    return person_service.get_persons(db)
